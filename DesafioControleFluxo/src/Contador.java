ec54901414378dec34dab49d5b1be43627bdfce2
Gustavo <gustavogla@gmail.com>
2024-08-20 16:01:51 -0300

    Meu projeto Desafio Controle de fluxo

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -1,9 +1,8 @@
 import java.util.Scanner;
 
-// Classe principal que inclui a exceção customizada
+
 public class Contador {
 
-    // Classe interna para a exceção customizada
     static class ParametrosInvalidosException extends Exception {
         public ParametrosInvalidosException(String mensagem) {
             super(mensagem);
@@ -13,7 +12,6 @@ public class Contador {
     public static void main(String[] args) {
         Scanner terminal = new Scanner(System.in);
 
-        // Capturando os parâmetros de entrada
         System.out.println("Digite o primeiro parâmetro");
         int parametroUm = terminal.nextInt();
 
@@ -21,10 +19,8 @@ public class Contador {
         int parametroDois = terminal.nextInt();
 
         try {
-            // Chamando o método contendo a lógica de contagem
             contar(parametroUm, parametroDois);
         } catch (ParametrosInvalidosException exception) {
-            // Imprimindo a mensagem de erro caso o segundo parâmetro seja menor que o primeiro
             System.out.println(exception.getMessage());
         } finally {
             terminal.close();
@@ -32,12 +28,10 @@ public class Contador {
     }
 
     static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
-        // Validar se parametroUm é MAIOR que parametroDois e lançar a exceção
         if (parametroUm > parametroDois) {
             throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
         }
 
-        // Calcular a quantidade de iterações e realizar o for
         int contagem = parametroDois - parametroUm;
         for (int i = 1; i <= contagem; i++) {
             System.out.println("Imprimindo o número " + i);
